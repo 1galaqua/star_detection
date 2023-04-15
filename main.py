6f90373174b0752b285ef86f2edde3bc6ec038fe
nirgeron <nirgeron@yahoo.com>
2023-04-15 18:29:55 +0300

    start trinangles

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     pic_to_csv("pic1", 'stars_images/image1.jpeg')
     pic_to_csv("pic2", 'stars_images/image2.jpeg')
 
+
---
+++ triangels.py
@@ -0,0 +1,34 @@
+import csv
+import math
+from itertools import combinations
+
+
+def read_points_from_csv(filename):
+    points = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip header row
+        for row in reader:
+            id, x, y = map(float, row[:3])
+            points.append((id, x, y))
+    return points
+
+
+def make_triangles(points):
+    angles = []
+    for triangle in combinations(points, 3):
+        id, x1, y1 = triangle[0]
+        id, x2, y2 = triangle[1]
+        id, x3, y3 = triangle[2]
+        angle1 = math.atan2(y2 - y1, x2 - x1)
+        angle2 = math.atan2(y3 - y2, x3 - x2)
+        angle3 = math.atan2(y1 - y3, x1 - x3)
+        angles.append((angle1, angle2, angle3))
+    return angles
+
+
+if __name__ == "__main__":
+    filename = "pic1.csv"
+    points = read_points_from_csv(filename)
+    angles = make_triangles(points)
+    print(angles)
